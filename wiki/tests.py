382c05354b810e39278afca4c719f65380bbd438
sandycamilo <scam499@lasierra.edu>
2020-02-24 15:30:21 -0800

    test

--- wiki/tests.py
+++ wiki/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class WikiTestCase(TestCase):
+    def test_true_is_true(self):
+        """ Tests is Trues is equal to True. Should always pass. """ 
+        self.assertEqual(True, True)
